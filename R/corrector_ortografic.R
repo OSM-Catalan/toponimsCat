c5b1bf4dd1a9c809b7773c15d6ee5b22968f08c1
Joan Maspons <joanmaspons@gmail.com>
2022-11-21 19:55:46 +0100

    Millora la funció revisionsSenseErrors i afegeix un paràmetre per deshabilitar regles de LanguageTool

--- R/corrector_ortografic.R
+++ R/corrector_ortografic.R
@@ -5,17 +5,20 @@
 #' @param fitxersRevisions camins de fitxers de revisions a processar.
 #' @param sufix_nou sufix pels fitxers de revisió nous sense els casos amb errors.
 #' @param LanguageTool si és \code{TRUE}, cerca errors també amb \code{\link[LanguageToolR]{languagetool}}. Pot ser força lent amb moltes dades.
+#' @param disabled_rules identificadors de regles de LanguageTool a deshabilitar.
 #'
-#' @return Camins dels fitxers nous sense els casos en què name:ca o alt_name:ca tenen errors ortogràfics segons el diccionari hunspell.
+#' @return Camins dels fitxers nous sense els casos en què \code{name:ca} o \code{alt_name:ca} tenen errors ortogràfics segons el diccionari hunspell (i LanguageTool).
 #' @export
 #'
 #' @examples
-revisionsSenseErrors<- function(fitxersRevisions, sufix_nou="_correcte.tsv", LanguageTool=FALSE){
+revisionsSenseErrors<- function(fitxersRevisions, sufix_nou="_correcte.tsv",
+                                LanguageTool=FALSE, disabled_rules=c("UPPERCASE_SENTENCE_START", "ESPAI_DARRERE_PUNTICOMA"), cl=1){
   if (!requireNamespace("hunspell", quietly=TRUE)){
     stop("La funció revisionsSenseErrors requereix el paquet «hunspell» per la detecció d'errors ortogràfics. Instal·leu el paquet i assegureu-vos que el diccionari català està instal·lat al sistema:\n\tintall.packages(\"hunspell\")", call.=FALSE)
   }
 
   sapply(fitxersRevisions, function(x){
+    message("\nS'està analitzant ", x)
     d<- utils::read.table(x, header=TRUE, sep="\t", quote="\"", check.names=FALSE)
     errors_name.ca<- hunspell::hunspell(as.character(d$`name:ca`), dict=hunspell::dictionary(lang="ca"))
     errors_alt_name.ca<- hunspell::hunspell(as.character(d$`alt_name:ca`), dict=hunspell::dictionary(lang="ca"))
@@ -25,24 +28,53 @@ revisionsSenseErrors<- function(fitxersRevisions, sufix_nou="_correcte.tsv", Lan
       return(NA_character_)
     }
 
-    if (LanguageTool && requireNamespace(LanguageToolR, quietly=TRUE)){
-      lt_name.caL<- lapply(as.character(dCorrecte$`name:ca`), LanguageToolR::languagetool, encoding="utf-8",
-                           linebreak_paragraph=FALSE, language="ca", disabled_rules=c(),
-                           enabled_rules=c(), enabled_only=FALSE, disabled_categories=c(),
-                           enabled_categories=c(), list_unknown=FALSE, apply=FALSE, quiet=TRUE)
-      lt_alt_name.caL<- lapply(as.character(dCorrecte$`alt_name:ca`), LanguageToolR::languagetool, encoding="utf-8",
-                           linebreak_paragraph=FALSE, language="ca", disabled_rules=c(),
-                           enabled_rules=c(), enabled_only=FALSE, disabled_categories=c(),
-                           enabled_categories=c(), list_unknown=FALSE, apply=FALSE, quiet=TRUE)
-      dCorrecte<- dCorrecte[sapply(lt_name.caL, nrow) == 0 & sapply(lt_alt_name.caL, nrow) == 0, ]
-    } else {
-      warning("S'ha omés la revisió amb «LanguateTool» perquè el paquet no és instal·lat. Instal·leu el paquet:\n\tremotes::install_github(\"nevrome/LanguageToolR\")")
+    if (LanguageTool){
+      if (requireNamespace("LanguageToolR", quietly=TRUE)){
+        message("\tLanguageTool for names")
+        lt_name.caL<- pbapply::pblapply(as.character(dCorrecte$`name:ca`), function(y){
+          if (y %in% c(NA, "")){
+            return(data.frame())
+          }
+          LanguageToolR::languagetool(y, encoding="utf-8", linebreak_paragraph=FALSE, language="ca",
+                                      disabled_rules=disabled_rules, enabled_rules=c(),
+                                      enabled_only=FALSE, disabled_categories=c(),
+                                      enabled_categories=c(), list_unknown=FALSE, apply=FALSE, quiet=TRUE)
+        }, cl=cl)
+        message("\n\tLanguageTool for alt_names")
+        lt_alt_name.caL<- pbapply::pblapply(as.character(dCorrecte$`alt_name:ca`), function(y){
+            if (y %in% c(NA, "")){
+              return(data.frame())
+            }
+            LanguageToolR::languagetool(y, encoding="utf-8", linebreak_paragraph=FALSE, language="ca",
+                                        disabled_rules=disabled_rules, enabled_rules=c(),
+                                        enabled_only=FALSE, disabled_categories=c(),
+                                        enabled_categories=c(), list_unknown=FALSE, apply=FALSE, quiet=TRUE)
+        }, cl=cl)
+        ## TODO: afegeix les dades de LT (lt_*name.caL) a d i permet filtrar a posteriori
+        lt_name.caL_df<- do.call(rbind, lt_name.caL[sapply(lt_name.caL, nrow) > 0])
+        if (!is.null(lt_name.caL_df)){
+          utils::write.table(lt_name.caL_df, file=gsub("\\.tsv$", "_LanguageToolERRORS-name:ca.tsv", x), sep="\t", na="", col.names=TRUE, row.names=FALSE)
+        }
+        lt_alt_name.caL_df<- do.call(rbind, lt_alt_name.caL[sapply(lt_alt_name.caL, nrow) > 0])
+        if (!is.null(lt_alt_name.caL_df)){
+          utils::write.table(lt_alt_name.caL_df, file=gsub("\\.tsv$", "_LanguageToolERRORS-alt_name:ca.tsv", x), sep="\t", na="", col.names=TRUE, row.names=FALSE)
+        }
+        dCorrecte<- dCorrecte[sapply(lt_name.caL, nrow) == 0 & sapply(lt_alt_name.caL, nrow) == 0, ]
+      } else {
+        warning("S'ha omés la revisió amb «LanguateTool» perquè el paquet no és instal·lat. Instal·leu el paquet:\n\tremotes::install_github(\"nevrome/LanguageToolR\")")
+      }
     }
 
-    fitxerNou<- gsub("\\.tsv$", sufix_nou, x)
-    utils::write.table(dCorrecte, file=fitxerNou, sep="\t", na="", col.names=TRUE, row.names=FALSE)
+    if (nrow(dCorrecte) > 0){
+      fitxerNou<- gsub("\\.tsv$", sufix_nou, x)
+      utils::write.table(dCorrecte, file=fitxerNou, sep="\t", na="", col.names=TRUE, row.names=FALSE)
 
-    return(fitxerNou)
+      return(fitxerNou)
+    } else {
+      message("No hi ha missatges sense errors d'ortografia")
+
+      return(NA)
+    }
 
     # TEST:
     # test<- data.frame(`name:ca`=d$`name:ca`, `err.name:ca`=sapply(errors_name.ca, paste, collapse="|"),
